de14ad1bfafe68d7e8a4cb8544b547d070a035c6
harsh <harsh.singh@skillup.tech>
2025-03-17 01:37:07 +0530

    update model

--- websites/src/main/java/com/websites/Websites.java
+++ websites/src/main/java/com/websites/Websites.java
@@ -1,10 +1,14 @@
 package com.websites;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Websites {
     
+    @JsonProperty("name")
     private String name;
+    
+    @JsonProperty("web_pages")
     private List<String> webPages;
 
     // Getters and setters
